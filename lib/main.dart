d6a3434f6ca64c6eaaad232a79fd9e88de896beb
inspiron14-windows <violetraisondetre@gmail.com>
2025-02-16 09:08:32 +0900

    feat:shooting_game

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,67 @@
 import 'package:flutter/material.dart';
 import 'package:flame/game.dart';
 import 'package:flame/components.dart';
+import 'package:flame/input.dart';
 
 void main() {
   final game = MyGame();
   runApp(GameWidget(game: game));
 }
 
-class MyGame extends FlameGame {
+class MyGame extends FlameGame with TapDetector {
+  late Player player;
+
   @override
   Future<void> onLoad() async {
     super.onLoad();
-    final square = RectangleComponent(
-      position: Vector2(100, 100),
-      size: Vector2(50, 50),
-      paint: Paint()..color = Colors.blue,
-    );
-    add(square);
+    player = Player()
+      ..position = Vector2(size.x / 2, size.y - 50)
+      ..size = Vector2(50, 50);
+    add(player);
+  }
+
+  @override
+  void onTap() {
+    final bullet = Bullet()
+      ..position = player.position.clone()
+      ..size = Vector2(5, 10);
+    add(bullet);
+  }
+}
+
+class Player extends PositionComponent {
+  @override
+  Future<void> onLoad() async {
+    super.onLoad();
+  }
+
+  @override
+  void render(Canvas canvas) {
+    super.render(canvas);
+    final paint = Paint()..color = Colors.blue;
+    canvas.drawRect(size.toRect(), paint);
+  }
+}
+
+class Bullet extends PositionComponent with HasGameRef<MyGame> {
+  @override
+  Future<void> onLoad() async {
+    super.onLoad();
+  }
+
+  @override
+  void render(Canvas canvas) {
+    super.render(canvas);
+    final paint = Paint()..color = Colors.red;
+    canvas.drawRect(size.toRect(), paint);
+  }
+
+  @override
+  void update(double dt) {
+    super.update(dt);
+    position.y -= 300 * dt;
+    if (position.y < 0) {
+      removeFromParent();
+    }
   }
 }
